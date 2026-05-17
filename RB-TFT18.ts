6d70c316b69041d80d739b6b3ef084c13c04e92b
Barry.C <wjyzxhm888@163.com>
2026-05-18 00:53:24 +0800

    更新 RB-TFT18.ts

--- RB-TFT18.ts
+++ RB-TFT18.ts
@@ -223,7 +223,7 @@ enum Color {
      //% x1.min=0 x1.max=127
      //% y1.min=0 y1.max=159
      //% weight=92
-     export function drawLine(x0: number, y0: number, x1: number, y1: number, color: Color = Color.Red): void {
+     export function drawLine(x0: number, y0: number, x1: number = 10, y1: number = 10, color: Color = Color.Red): void {
          let xDelta = x1 - x0
          let yDelta = y1 - y0
 
@@ -260,7 +260,7 @@ enum Color {
      //% x.min=0 x.max=127
      //% y.min=0 y.max=159
      //% weight=90
-     export function drawRectangle(x: number, y: number, width: number, height: number, color: Color = Color.Red): void {
+     export function drawRectangle(x: number, y: number, width: number = 10, height: number = 10, color: Color = Color.Red): void {
 
          // Convert color
          let hiColor = (color >> 8) % 256
@@ -287,7 +287,7 @@ enum Color {
      //% x.min=0 x.max=127
      //% y.min=0 y.max=159
      //% weight=88
-     export function drawCircle(x: number, y: number, radius: number, color: Color = Color.Red): void {
+     export function drawCircle(x: number, y: number, radius: number = 10, color: Color = Color.Red): void {
         for(let y1 = -radius ; y1 <= 0 ; y1++) {
             for(let x1 = -radius ; x1 <= 0 ; x1++) {
                 if((x1 * x1 + y1 * y1) <= (radius * radius)) {
